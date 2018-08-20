092c7e5140092e3f5798dd6a082d3c496eab3ae5
Niektóry <niektory@gmail.com>
2018-08-20 13:40:38 +0200

    Make URLs with accented characters work

--- functions.php
+++ functions.php
@@ -7,3 +7,8 @@ add_action( 'init', 'register_my_menu' );
 
 add_theme_support( 'custom-header' );
 add_theme_support( 'automatic-feed-links' );
+
+function remove_accents_from_url( $title ) {
+	return urlencode( remove_accents( urldecode( $title ) ) );
+}
+add_filter( 'sanitize_title', 'remove_accents_from_url' );
